a1a6b770a7d89ad2aaa73e240adfbfc49d66be76
Alon Blich <Alon.Blich@progress.com>
2017-02-17 17:06:29 +0000

    Changes made by Alon Blich (Alon.Blich@progress.com).

--- Offline App/dataSources/EmployeeDataSource.js
+++ Offline App/dataSources/EmployeeDataSource.js
@@ -8,7 +8,6 @@
                         return this.LastName + ', ' + this.FirstName;
                     },
 
-
                     ImageURI: function () {
                         return "img/" + (this.EmpNum <= 10 ? this.EmpNum : "unknown") + ".jpg";
                     }
